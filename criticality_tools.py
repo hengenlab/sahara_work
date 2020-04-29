d32db43c380f1a17163453172d65f3dc41f124b0
sahara <sahara@Saharas-MacBook-Pro.local>
2020-04-29 12:10:30 -0500

    got rid of the matrix function, breaking up manually

--- criticality_tools.py
+++ criticality_tools.py
@@ -5,14 +5,36 @@ import matplotlib
 # matplotlib.use('Agg')
 import matplotlib.pyplot as plt 
 
+
+def FR_plot(cells, binsz, rec_len, color=False):
+    plt.ion()
+    fig,ax = plt.subplots(nrows=1, ncols=1)
+    fig.set_figheight(3)
+    fig.set_figwidth(12)
+
+    total_sps = rec_len*3600*25000 # total sample points. rec_len should be in hours
+    bin_sps = binsz*25000 # total sample points in each bin, binsz should be in seconds
+
+    bins = np.arange(0, (total_sps + bin_sps), bin_sps) # creates the bins for the firing rate plot. takes total recording length and breaks it up by binsize
+    
+    colors=['seagreen', 'gold', 'salmon']
+    for cell in cells:
+        spk_times=cells.spike_time
+        fr=np.histogram(spk_times, bins=bins)
+        fr=fr[0]/600
+        a
+
+
+
 """
 takes in an array of dictionaries and pulls all the dcc and p_value data, appends all that data into a nice block and saves it where you want
 """
 def pull_crit_data(all_dicts, save_loc, animal, time_frame, paths=False):
     all_dicts_obj=[]
+
     if paths:
         for path in all_dicts:
-            print(f'loading dict: {path}')
+            print (f'loading dict: {path}')
             all_dicts_obj.append(np.load(path, allow_pickle=True).item())
     else:
         all_dicts_obj=all_dicts
@@ -35,12 +57,13 @@ def pull_crit_data(all_dicts, save_loc, animal, time_frame, paths=False):
     all_data = [all_dccs, all_p_b, all_p_t]
 
     np.save(save_loc+f"/dcc_pb_pt_{animal}_{time_frame}", all_data)
+    np.save(save_loc+f'/all_params_{animal}_{time_frame}', all_params)
     return all_data, all_params
 
 params={
     "animal": "caf19",
-    "date": "0326",
-    "time_range":"0-288"
+    "date": "0409",
+    "time_range":"108-168"
 }
 """
 makes pretty plots from arrays of all dcc and p value data. not from the dictionaries - make sure theres an extra 0 in the labels
@@ -88,6 +111,8 @@ def break_up_mat(FR_mat, small_bin, hour_bins):
     # small_bin: original size of bin used to make the matrix, in seconds
     # hour_bins: how you want the matrix broken up, in hours 
         # ex: 4 - would break up a 12 hour matrix into 3 arrays of 4 hours
+
+
     small_bin=small_bin*1000
     total_time = FR_mat.shape[1]*(small_bin) # in ms
     num_cells = FR_mat.shape[0]
@@ -151,17 +176,26 @@ def looped_crit(FR_mat, params, plot=True):
     burstM=params['burstM']
     tM = params['tM']
     hour_bins=params['hour_bins']
+    total_time=params['total_time']
     time_frame = params["time_frame"]
     animal = params["animal"]
 
+    num_bins = int(total_time/hour_bins)
+    bin_len = int((hour_bins*3600)/ava_binsz)
 
     master_dict = {}
     all_p_values_burst = []
     all_p_values_t=[]
     all_dcc_values = []
-    for idx, t_bin in enumerate(FR_mat):
-        print(f"working on block {idx+1} of {len(FR_mat)}")
-        Result = cr.AV_analysis_BurstT(t_bin, perc=perc)
+    for idx, t_bin in enumerate(np.arange(0, num_bins)):
+        print(f"working on block {idx+1} of {num_bins}")
+        
+        if idx == num_bins-1:
+            data=FR_mat[:, (idx*bin_len):]
+        else:
+            data=FR_mat[:, (idx*bin_len) : ((idx+1)*bin_len)]
+
+        Result = cr.AV_analysis_BurstT(data, perc=perc)
         Result2, ax1, ax2 = cr.AV_analysis_ExponentErrorComments(Result["S"], Result["T"], burstM, tM, time_frame+'_'+str(idx), flag = 2, EX_burst=1, EX_time=1)
         Result3, ax3 = cr.AV_analysis_ExponentErrorComments(Result["S"], Result["T"], burstM, tM, time_frame+'_'+str(idx), flag = 3)
         master_dict["Result_block"+str(idx)] = Result2
@@ -209,13 +243,14 @@ def looped_crit(FR_mat, params, plot=True):
 params = {
     'ava_binsz': 0.04,
     'hour_bins': 4,
+    'total_time':16,
     'perc': 0.35,
     'burstM': 18,
     'tM': 4,
     'quality': [1],
-    'time_frame': '0326_0_16',
+    'time_frame': '0409',
     'animal' : 'caf19',
-    'notes': ''
+    'notes': 'new recording, trying new params'
 }
 
 
@@ -244,6 +279,7 @@ def lilo_and_stitch(paths, params, overlap=0, plot=False):
     burstM=params['burstM']
     tM = params['tM']
     hour_bins=params['hour_bins']
+    total_time=params['total_time']
     time_frame = params["time_frame"]
     animal = params["animal"]
 
@@ -257,16 +293,16 @@ def lilo_and_stitch(paths, params, overlap=0, plot=False):
         
         cells = np.load(path, allow_pickle=True)
         good_cells = [cell for cell in cells if cell.quality in quality]
-        fr_mat = mbt.n_spiketimes_to_spikewords(good_cells, binsz=ava_binsz, binarize=1)
+        data = mbt.n_spiketimes_to_spikewords(good_cells, binsz=ava_binsz, binarize=1)
         
-        if overlap:
-            ms_in_overlap = overlap * 3600 * 1000
-            bins_in_overlap = ms_in_overlap/(ava_binsz*1000)
-            fr_mat_trimmed = fr_mat[:, 0:-int(bins_in_overlap)]
-            data = break_up_mat(fr_mat_trimmed, ava_binsz, hour_bins)
-        else:
-            fr_mat_reshaped = break_up_mat(fr_mat, ava_binsz, hour_bins)
-            data = fr_mat_reshaped
+        # if overlap:
+        #     ms_in_overlap = overlap * 3600 * 1000
+        #     bins_in_overlap = ms_in_overlap/(ava_binsz*1000)
+        #     fr_mat_trimmed = fr_mat[:, 0:-int(bins_in_overlap)]
+        #     data = break_up_mat(fr_mat_trimmed, ava_binsz, hour_bins)
+        # else:
+        #     fr_mat_reshaped = break_up_mat(fr_mat, ava_binsz, hour_bins)
+        #     data = fr_mat_reshaped
         
         master_dict = looped_crit(data, params, plot=False)
         np.save(base_path+animal+"_dict_"+str(hour_bins)+"hrs_"+time_frame, master_dict)
