5de7443116295eb3b5c206e14dc566b6e9394e34
Saharae <saharae27@gmail.com>
2020-06-05 13:33:18 -0500

    switched back from hardcoding boundary

--- Criticality/Code_Comments/pvaluenew.m
+++ Criticality/Code_Comments/pvaluenew.m
@@ -1,14 +1,16 @@
 function [P_value ks]= pvaluenew(burst)
+
 % Null hypothesis test. When P_value is very large, we could not reject
 % the Null hypothesis that the distribution of burst follows power law.
 % Usually, we use 0.05 as criteria.
 
-[alpha xmin] = tplfit(burst,40);%max(5,min(burst)))
+[alpha xmin] = tplfit(burst,40); %max(5,min(burst)))
 xmax = max(burst);
 N = length(burst);
 k = 0;
 z = burst;
-z = z(z>=xmin);            n    = length(z);
+z = z(z>=xmin);            
+n    = length(z);
 cdf = cumsum(hist(z,xmin:xmax)./n);
 
 s = unique(burst(xmin<=burst & burst<=xmax));
@@ -16,7 +18,8 @@ smin = min(s);      %smax = max(s);  s = smin:smax;
 A = 1/ sum(s.^-alpha );
 fit = cumsum ( A*(xmin:xmax).^-alpha );
 KS = max(abs(cdf - fit))
-ks = [];        j = 1;
+ks = [];        
+j = 1;
 h = figure; semilogx(xmin:xmax,fit,'*',xmin:xmax,cdf,'+r'); hold on; xlabel('Size','Fontsize',16);   ylabel('Prob(size < S)','Fontsize',16);
 title('CDF,field view # ','Fontsize',17);
 A = lognfit(z); mu = A(1); sig = A(2);
@@ -40,11 +43,13 @@ while j< Niter
     % % xmax = max(burst); N = length(burst(burst>xmin));
     % xmin=1;
     % generate a data set following power law with the same exponent.
+
     N = 20*length(burst(burst>=xmin)); % generate N data points
     syn_data = floor( (xmin-1/2)*(1-rand(1,N)).^(1/(1-alpha)) + 1/2 ) ;
     syn_data = floor(heaviside(xmax-syn_data)) .* syn_data;
     syn_data(syn_data==0)=[];
     syn_data = syn_data(1:length(burst(burst>=xmin))); % choose the same size as burst
+   
     %% ########################## Accept-Reject Method  #############################################
     % N = 2*length(burst(burst>=xmin));
     % beta = alpha - 1;
@@ -61,7 +66,8 @@ while j< Niter
     n = length(syn_data(xmin<=syn_data & syn_data<=xmax)) ; 
     a = tplfit(X,xmin); % calculate exponent for surrogated data    
     if abs(a-alpha)<=0.1 & a >1.0   % Make sure we use the right surrogated data
-        X = X(X>=xmin);            n    = length(X);
+        X = X(X>=xmin);            
+        n    = length(X);
         cdf = cumsum(hist(X,xmin:xmax)./n);
         s = unique(X(xmin<=X & X<=xmax));
         smin = min(s);      smax = max(s);
