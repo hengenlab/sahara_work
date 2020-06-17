b7607604375f25b4b3f5dbeb543eb4b17af2b7b1
Saharae <saharae27@gmail.com>
2020-06-17 16:54:51 -0500

    rewritten crit code

---
+++ Criticality_new/AV_analysis_new.py
@@ -0,0 +1,127 @@
+import numpy as np
+from sahara_work import Criticality_new as cr
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def scaling_plots(burst, burstMin, burstMax, alpha, T, tMin, beta, TT, Sm, sigma, fit_sigma, plt_name, saveloc):
+    # burst PDF
+    fig1, ax1 = plt.subplots(nrows = 1, ncols = 3, figsize = [10, 6])
+    pdf = np.histogram(burst, bins = np.arange(1, np.max(burst)+2))[0]
+    ax1[0].plot(np.arange(1,np.max(burst)+1),pdf/np.sum(pdf), marker = 'o', markersize = 3, linestyle = 'None', color = '#2138ab', alpha = 0.75)
+    ax1[0].set_yscale('log')
+    ax1[0].set_xscale('log')
+
+    x = np.arange(burstMin, burstMax+1)
+    y = (np.size(np.where(burst == burstMin)[0])/np.power(burstMin, -alpha))*np.power(x, -alpha)
+    y = y/np.sum(pdf)
+
+    ax1[0].plot(x,y, color = '#c5c9c7');
+    ax1[0].set_xlabel('AVsize')
+    ax1[0].set_ylabel('PDF(D)')
+    ax1[0].set_title('AVsize PDF, ' + str(np.round(alpha[0], 3)))
+
+    # time pdf
+    tdf = np.histogram(T,bins = np.arange(1, np.max(T)+2))[0]
+    ax1[1].plot(np.arange(1,np.max(T)+1),tdf/np.sum(tdf), marker = 'o', markersize = 3, linestyle = 'None', color = '#7bfdc7', alpha = 0.75)
+    ax1[1].set_yscale('log')
+    ax1[1].set_xscale('log')
+    sns.despine()    
+
+    x = np.arange(tMin,tMax+1)
+    y = np.size(np.where(T == tMin))/(np.power(tMin,-beta))*np.power(x,-beta)
+	y = y/np.sum(tdf)
+    ax1[1].plot(x,y, color = '#c5c9c7')
+    ax1[1].set_xlabel('AVduration')
+    ax1[1].set_ylabel('PDF(D)')
+    ax1[1].set_title('AVdura PDF, ' + str(np.round(beta[0], 3))) 
+
+    # figure out how to plot shuffled data
+
+    #scaling relation 
+    ax1[2].plot(TT, ((np.power(TT,sigma)/np.power(TT[7],sigma))*Sm[7]), label = 'pre', color = '#4b006e')
+    ax1[2].plot(TT, (np.power(TT,fit_sigma[0])/np.power(TT[7],fit_sigma[0])*Sm[7]),'b', label = 'fit', linestyle = '--', color = '#826d8c')
+    ax1[2].plot(TT, Sm,'o', color = '#fb7d07', alpha = 0.75)
+    ax1[2].set_xscale('log')
+    ax1[2].set_yscale('log')
+    ax1[2].set_ylabel('<S>')
+    ax1[2].set_title('Difference = ' + str(np.round(Result['df'][0], 3)))
+
+	plt.tight_layout()
+	plt.legend()
+	plt.savefig(saveloc+"/"+pltname+'scaling_relations')
+
+    return fig1
+
+def AV_analysis_new(burst, T, bm, tm, pltname, saveloc, flag = 1, burst_shuffled=None, T_shuffled=None, plot_shuffled=False, plot=True):
+    burstMax, burstMin, alpha = cr.EXCLUDE(burst, bm)
+    burst_idx = np.where(np.logical_and(burst<=burstMax, burst>=xmin))[0]
+
+    alpha, xmin, ks, L = cr.tplfit(burst[idx_burst], burstMin)
+
+    print("burst min: ", burstMin)
+	print("xmin: ", xmin)
+	print("burst max:", burstMax)
+
+	Result['burst'] = burst
+	Result['alpha'] = alpha
+	Result['xmin'] = burstMin
+	Result['xmax'] = burstMax
+
+    if flag == 2 :
+        # pvalue test
+        Result['P_burst'], ks, hax_burst, ptest_bmin  = cr.pvaluenew(burst[idx_burst], burstMin)
+
+    tMax, tMin, beta = cr.EXCLUDE(T[T < np.power(np.max(T),0.8)], tm)
+    idx_time = np.where(np.logical_and(T >= tMin,T <= tMax + 1))[0]
+    beta, new_tmin, tplfit_ks_time, L = cr.tplfit(T[idx_time], tMin)
+    
+    print(f'time min: {tMin}')
+	print(f'new tmin: {new_tmin}')
+	print(f'time max: {tMax}')
+
+    Result['T'] = T
+	Result['beta'] = beta
+	Result['tmin'] = tMin
+	Result['tmax'] = tMax
+
+    if flag == 2:
+        #pvalue for time
+        Result['P_t'], ks, hax_time, ptest_tmin  = cr.pvaluenew(T[idx_time],tMin)
+   
+    # scaling relation 
+    TT = np.arange(1, np.max(T)+1)
+	Sm = []
+	for i in np.arange(0,np.size(TT)):
+		Sm.append(np.mean(burst[np.where(T==TT[i])[0]]))
+	Sm = np.asarray(Sm)
+	Loc=np.where(Sm>0)[0]
+	TT=TT[Loc]
+	Sm=Sm[Loc]
+
+    fit_sigma = np.polyfit(np.log(TT[np.where(np.logical_and(TT>tMin, TT<tMax))[0]]), np.log(Sm[np.where(np.logical_and(TT>tMin, TT<tMax))[0]]), 1);
+	sigma = (beta - 1)/(alpha - 1)
+
+	Result['pre'] = sigma
+	Result['fit'] = fit_sigma
+	Result['df'] = np.abs(sigma - fit_sigma[0])
+	Result['TT'] = TT
+	Result['Sm'] = Sm
+
+    m = fit_sigma[0]
+    c = fit_sigma[1]
+
+    if plot:
+        fig1 = scaling_plots(burst, burstMin, burstMax, alpha, T, tMin, beta, TT, Sm, sigma, fit_sigma, plt_name, saveloc)
+        if flag == 2 :
+            hax_burst.axes[0].set_xlabel('Size (S)', fontsize = 16)
+            hax_burst.axes[0].set_ylabel('Prob(size < S)', fontsize = 16)
+            hax_burst.savefig(saveloc+"/"+pltname+'pvalue_burst')
+
+            hax_time.axes[0].set_xlabel('Duration (D)', fontsize = 16)
+            hax_time.axes[0].set_ylabel('Prob(size < D)', fontsize = 16)
+            hax_time.savefig(saveloc+"/"+pltname+'pvalue_time')
+            Result['burst_cdf'] = hax_burst
+            Result['time_cdf'] = hax_time
+        Result['scaling_relation_plot'] = fig1
+
+    return Result
---
+++ Criticality_new/pvaluenew2.py
@@ -0,0 +1,71 @@
+import numpy as np
+from sahara_work import Criticality_new as cr
+from copy import deepcopy as cdc
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import seaborn as sns
+import math
+
+def pvaluenew(burst, alpha, xmin, ks, Loglike):
+    xmax = np.max(burst) # not truly the max, cause we pass in the truncated dataset
+    n   = np.size(burst)
+    cdf = np.cumsum(np.histogram(z,np.arange(xmin,xmax+2))[0]/n) 
+    s = np.unique(burst)
+    smin = np.min(s)
+	A = 1/np.sum(np.power(s, -alpha[0]))
+	fit = np.cumsum(A*np.power(np.arange(xmin,xmax+1), -alpha[0]))
+	KS = np.max(np.abs(cdf-fit))
+    
+
+    hax.plot(np.arange(xmin,xmax+1), fit, zorder = 10000, label = 'Power law CDF', color = '#0504aa')
+	hax.plot(np.arange(xmin,xmax+1), cdf, zorder = 10005, label = 'Experimental CDF', color = '#80013f')
+	hax.legend()
+	hax.set_title('Cumulative Distribution Function ',fontsize = 12)
+	hax.set_xscale('log')
+	shape, loc, scale = stats.lognorm.fit(z, floc=0)
+	sig = shape
+	mu = math.log(scale)
+	sns.despine()
+
+    ks = []
+	j = 1
+	Niter = 1000
+
+    N=10*np.size(burst)
+
+	while j<Niter:
+ 		if not j % 400:
+			print(str(j) + " loops completed")
+
+        syn_data = np.floor((xmin-1/2)*np.power((1-np.random.uniform(0,1,N)), (1/(1-alpha[0]))) + 1/2)
+        syn_data = np.floor(np.heaviside(xmax-syn_data, 1/2) * syn_data)
+        syn_data = np.delete(syn_data, np.where(syn_data == 0)[0])
+        syn_data = syn_data[0:n]
+        idx_syn = np.where(np.logical_and(xmin<=syn_data, syn_data<=xmax))[0]
+		X = syn_data[idx_syn]
+		alpha_syn, xmin_syn, ks_syn, Loglike_syn = cr.tplfit(syn_data,xmin) #calculate exponent for surrogated data 
+		a = alpha_syn[0]   
+
+        if np.abs(a-alpha[0])<=0.1 and a >1.0:
+            size = np.size(X)
+            cdf = np.cumsum(np.histogram(X,np.arange(xmin,xmax+2))[0]/size)
+            s = np.unique(X)
+            smin = min(s)    
+			smax = max(s)
+            A = 1/np.sum(np.power(s,-alpha[0]))
+            fit = np.cumsum(A*np.power(np.arange(xmin,xmax+1), -alpha[0]))
+            ks.append(np.max(np.abs(cdf - fit)))
+            j = j + 1
+            hax.plot(np.arange(xmin,xmax+1), cdf, color = '#647d8e', alpha  = 0.05, linewidth = 0.3)
+
+    ks = np.asarray(ks)
+    P_value = np.sum(np.sign(ks[ks>=KS]))/Niter
+
+    ylim = hax.get_ylim()[-1]
+    xlim = hax.get_xlim()[-1]
+    txt_b = hax.text(xlim*0.5, ylim*0.5, 'p = ' + str(P_value), fontsize = 15)
+	
+    print('P_value = ' + str(P_value))
+	print('KS = ' + str(KS))
+
+    return P_value, ks, hfig, xmin
