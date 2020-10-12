f43ab4d8a59fee97a03a0b7cdd95278406ed6cf2
saharae <saharae27@gmail.com>
2020-10-12 17:31:02 -0500

    shell update

--- crit.sh
+++ crit.sh
@@ -1,2 +1,3 @@
-python3 big_crit.py caf22 True &> media/HlabShare/clayton_sahara_work/criticality/crit_out.txt
-python3 big_crit.py &> media/HlabShare/clayton_sahara_work/criticality/crit_out.txt
+python3 big_crit.py eab47 probe2 True &> media/HlabShare/clayton_sahara_work/criticality/crit_out_eab47.txt &
+python3 big_crit.py caf40 probe1 True &> media/HlabShare/clayton_sahara_work/criticality/crit_out_caf40.txt &
+python3 big_crit.py caf42 probe3 True &> media/HlabShare/clayton_sahara_work/criticality/crit_out_caf42.txt &
