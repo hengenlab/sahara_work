998b6ec6c276b055e4768f36c0b2b4e6f759324a
Saharae <saharae27@gmail.com>
2020-11-30 09:29:32 -0600

    bug fix with load

--- crit_class.py
+++ crit_class.py
@@ -424,16 +424,16 @@ def get_results(animal,probe='', paths = None, save=False, saveloc='', re_load =
             gc.collect()
         try:
             crit = None
-            with np.load(p, allow_pickle=True) as crit:
-                crit = crit[0]
+            np.load(p, allow_pickle=True)
+            crit = crit[0]
+            try:
+                results.append([crit.animal, crit.probe, crit.date, crit.time_frame, crit.block_num, crit.p_value_burst, crit.p_value_t, crit.dcc, (crit.p_value_burst > 0.05 and crit.p_value_t > 0.05), crit.kappa_burst, crit.kappa_t, crit.k2b, crit.k2t, crit.kprob_b, crit.kprob_t])
+            except Exception:
                 try:
-                    results.append([crit.animal, crit.probe, crit.date, crit.time_frame, crit.block_num, crit.p_value_burst, crit.p_value_t, crit.dcc, (crit.p_value_burst > 0.05 and crit.p_value_t > 0.05), crit.kappa_burst, crit.kappa_t, crit.k2b, crit.k2t, crit.kprob_b, crit.kprob_t])
-                except Exception:
-                    try:
-                        results.append([crit.animal, probe, crit.date, crit.time_frame, crit.block_num, crit.p_value_burst, crit.p_value_t, crit.dcc, (crit.p_value_burst > 0.05 and crit.p_value_t > 0.05), crit.kappa_burst, crit.kappa_t, crit.k2b, crit.k2t, crit.kprob_b, crit.kprob_t])
-                    except Exception as er:
-                        print(f"not going to work --- skipping this path {p}")
-                        errs.append([p, er])
+                    results.append([crit.animal, probe, crit.date, crit.time_frame, crit.block_num, crit.p_value_burst, crit.p_value_t, crit.dcc, (crit.p_value_burst > 0.05 and crit.p_value_t > 0.05), crit.kappa_burst, crit.kappa_t, crit.k2b, crit.k2t, crit.kprob_b, crit.kprob_t])
+                except Exception as er:
+                    print(f"not going to work --- skipping this path {p}")
+                    errs.append([p, er])
         except Exception as er:
             print("won't load object")
             good=False
