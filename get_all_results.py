f4f8c2ca1f7af1f295114ed22c617b7b3370785d
saharae <saharae27@gmail.com>
2020-12-02 14:33:15 -0600

    update to allow to pickup where it leftoff

--- get_all_results.py
+++ get_all_results.py
@@ -4,12 +4,12 @@ import csv
 import os
 
 
-def get_all_results(save = True, csvloc = '', re_load=False):
+def get_all_results(save, csvloc, loaded_file, re_load):
     '''
     because python refuses to play nice with memory. This will load all crit info into 
     a csv to be loaded into a pandas df at a later time
 
-    fingers crossed this works
+    it doesn't really work. Gonna eventually re-write this correctly but for now - a bandaid
     '''
     paths = glob.glob(f'/media/HlabShare/clayton_sahara_work/criticality/*/*/*/Crit*')
     print(f'Total # of paths: {len(paths)}')
@@ -19,24 +19,30 @@ def get_all_results(save = True, csvloc = '', re_load=False):
         with open(csvloc, 'w', newline='') as c:
             w =  csv.DictWriter(c, fieldnames=cols)
             w.writeheader()
+        loaded = []
+    else:
+        loaded = np.load(loaded_file)
+        print(f'Number of paths already loaded: {len(loaded)}')
 
     for i, p in enumerate(paths):
         if i%5 == 0:
             print(f'#paths: {i}', flush = True)
 
-        if 'LOADED' not in p or re_load:
+        if i%100 == 0 and i!=0:
+            print('saving progress', flush = True)
+            np.save(loaded_file, loaded)
+
+        if p not in loaded or re_load:
             err, to_append = s.lil_helper_boi(p)
             if err:
                 print(to_append)
                 errs.append(to_append)
             else:
                 s.write_to_csv(to_append, cols, csvloc)
+                loaded.append(p)
 
             if 'LOADED' in p:
                 base = p[:p.find('_LOADED')]
-            else:
-                base = p[:-4]
-            n = base+'_LOADED.npy'
-            os.rename(p, n)
+                os.rename(p, base+'.npy')
             
-    return errs
+    return errs, loaded
