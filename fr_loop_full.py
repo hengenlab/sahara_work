8ae85f9687230c3ef1e89fd133c2231465225bf1
saharae <saharae27@gmail.com>
2021-01-30 12:19:19 -0600

    added more paths for keith

--- fr_loop_full.py
+++ fr_loop_full.py
@@ -25,13 +25,18 @@ def cv_isi(start, stop, times):
     return cv
 
 bigtic = time.time()
-geno = ['wt']
+geno = ['te4']
 
 paths = s.get_paths(scorer = 'xgb', geno = geno)
 print(f'num paths: ', len(paths))
+
 if 'wt' in geno:
     additional_paths = glob.glob('/media/bs007s/caf/caf69/caf69_*/*/probe4/co/*neurons_group0.npy')
     paths = np.concatenate([paths, additional_paths])
+if 'te4' in geno:
+    additional_paths = glob.glob('/media/bs007s/caf/caf62/caf62_*/*/probe1/co/*neurons_group0.npy')
+    paths = np.concatenate([paths, additional_paths])
+
 
 print(f'num total Paths: ', len(paths))
 # Subset of the full dataset for testing (comment out when not needed)
