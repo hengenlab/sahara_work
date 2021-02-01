77e405f4f5c13e50cc91e2db94c642f16bb6b1cb
saharae <saharae27@gmail.com>
2021-02-01 16:56:58 -0600

    useless functions out of init

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-from .criticality_plots import *
-from .criticality_tools import *
+#from .criticality_plots import *
+#from .criticality_tools import *
 from .crit_class import *
 from .crit_utils import *
 from .crit_hlab import *
